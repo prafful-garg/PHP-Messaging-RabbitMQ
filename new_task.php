ac802082d2789374e98841febfe720992902187e
Prafful Garg <prafful.garg@nagarro.com>
2015-06-02 17:20:31 +0530

    Initial Commit

---
+++ new_task.php
@@ -0,0 +1,17 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+$connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+$channel->queue_declare('task_queue1', false, true, false, false);
+$data = implode(' ', array_slice($argv, 1));
+if(empty($data)) $data = "Hello World!";
+$msg = new AMQPMessage($data,
+                        array('delivery_mode' => 2) # make message persistent
+                      );
+$channel->basic_publish($msg, '', 'task_queue1');
+echo " [x] Sent ", $data, "\n";
+$channel->close();
+$connection->close();
+?>
